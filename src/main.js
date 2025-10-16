cb44fda69a83fbfd58581213ebd973755f9ca69b
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-10-17 02:17:41 +0530

    Changing to mak edesktop application start

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import kaplay from 'kaplay';
+
+//letterbox: true means the game will scale irrespective of the screen size
+//while maintaining the aspect ratio
+const k=kaplay({
+    width:1280,
+    height:720,
+    letterbox:true,
+    global:false,
+    scale:2
+});
