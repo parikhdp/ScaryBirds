43847d4421a7c7387a0435d15dc67afb7efcfa96
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-10-22 12:58:34 +0530

    Adding windows full screen & permissions to read and write to the file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    scarybirds_lib::run()
+    scarybirds_lib::run();
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
