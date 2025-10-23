b720ecbb6c96d0be621ee0500ffe8ccebc87953b
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-10-23 16:09:47 +0530

    starting with the player logic

--- src/jackie.js
+++ src/jackie.js
@@ -1,7 +1,24 @@
 //writing main player logic here
+import { SCALE_FACTOR } from './constants.js';
 
 export function makePlayer(k){
     return k.make([
         k.sprite("jackie"),
-    ])
+        k.area({
+            shape:new k.Rect(k.vec2(0,1.5),8,5)
+        }),
+        k.anchor("center"),
+        k.scale(SCALE_FACTOR),
+        {
+            isDead:false,
+            speed: 600,
+            keyControllers:[],
+            setControls(){
+                const jumpLogic=()=>{
+                    k.play("jump"),
+                    this.jump();
+            }
+        }
+    }
+    ]);
 }
